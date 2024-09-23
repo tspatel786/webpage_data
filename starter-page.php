e33891ea73ff4e9e316d8fa5c6dce13996f8139f
taufikpatel <taufikpatel23@gmail.com>
2024-09-23 08:40:17 +0530

    smpatel

---
+++ starter-page.php
@@ -0,0 +1,35 @@
+
+<?php
+include'header.php'
+?>
+
+
+  <main class="main">
+
+    <!-- Page Title -->
+    <div class="page-title dark-background" style="background-image: url(assets/img/page-title-bg.jpg);">
+      <div class="container position-relative">
+        <h1>Starter Page</h1>
+        <nav class="breadcrumbs">
+          <ol>
+            <li><a href="index.html">Home</a></li>
+            <li class="current">Starter Page</li>
+          </ol>
+        </nav>
+      </div>
+    </div><!-- End Page Title -->
+
+    <!-- Starter Section Section -->
+    <section id="starter-section" class="starter-section section">
+
+      <div class="container" data-aos="fade-up">
+        <p>Use this page as a starter for your own custom pages.</p>
+      </div>
+
+    </section><!-- /Starter Section Section -->
+
+  </main>
+
+  <?php
+   include'footer.php'
+   ?>
